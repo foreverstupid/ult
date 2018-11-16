90717bf05cbef4aa2109c20aeb5b3ab32ae51327
foreverstupid <nikolaev.mihail@inbox.ru>
2018-11-17 02:49:47 +0300

    Base text operations are added

---
+++ src/headers/text_operation.hpp
@@ -0,0 +1,43 @@
+#ifndef TEXT_OPERATIONS_MODULE_HPP
+#define TEXT_OPERATIONS_MODULE_HPP
+
+/*
+ * Checks if a character is a digit
+ */
+inline bool isDigit(char ch)
+{
+    return ch >= '0' && ch <= '9';
+}
+
+/*
+ * Checks if a character ends a line
+ */
+inline bool isEndOfLine(int ch)
+{
+    return ch == '\n' || ch == EOF;
+}
+
+/*
+ * Checks if a charcter is a space
+ */
+inline bool isSpace(int ch)
+{
+    return ch == '\t' || ch == ' ' || ch == '\r' || isEndOfLine(ch);
+}
+
+/*
+ * Checks if a string is an integer number
+ */
+bool isNumber(const char *str);
+
+/*
+ * Converts a string to an integer number
+ */
+int str2int(const char *str);
+
+/*
+ * Converts a string to a float number
+ */
+double str2double(const char *str);
+
+#endif
---
+++ src/text_operations.cpp
@@ -0,0 +1,90 @@
+#include "headers/text_operations.hpp"
+
+bool isNumber(const char *str)
+{
+    int i = 0;
+    bool wasPoint = false;
+
+    if(str[0] == '-' || str[0] == '+'){
+        i = 1;
+    }
+
+    for(; str[i]; i++){
+        if(str[i] == '.'){
+            if(!wasPoint){
+                wasPoint = true;
+                continue;
+            }else{
+                return false;
+            }
+        }
+
+        if(str[i] < '0' || str[i] > '9'){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+int str2int(const char *str)
+{
+    int res = 0;
+    int i = 0;
+    int sign = 1;
+
+    if(!str){
+        return 0;
+    }
+
+    if(str[0] == '-'){
+        sign = -1;
+        i = 1;
+    }else if(str[0] == '+'){
+        i = 1;
+    }
+
+    for(; str[i]; i++){
+        res *= 10;
+        res += str[i] - '0';
+    }
+
+    return res * sign;
+}
+
+
+
+double str2double(const char *str)
+{
+    int i = 0;
+    int sign = 1;
+    double res = 0.0;
+    double hlp = 0.1;
+
+    if(!str){
+        return 0.0;
+    }
+
+    if(str[0] == '-'){
+        sign = -1;
+        i = 1;
+    }else if(str[0] == '+'){
+        i = 1;
+    }
+
+    for(; str[i] && str[i] != '.'; i++){
+        res *= 10;
+        res += str[i] - '0';
+    }
+
+    if(str[i] == '.'){
+        for(++i; str[i]; i++){
+            res += hlp * (str[i] - '0');
+            hlp /= 10;
+        }
+    }
+
+    return res * sign;
+}
